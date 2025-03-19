b4b9d80eb09cecd5f2c97bd99edfba0f62fd6992
Lukas <lkfernando12345@gmail.com>
2025-03-19 16:11:36 -0300

    feat Task 48: criando o endpoint que retorna informações das arvores

--- app/controllers/area_information.py
+++ app/controllers/area_information.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import jwt_required
 from flask import Blueprint, abort, request, jsonify
 from flasgger import swag_from
 
+from datetime import datetime
 from app.util.messages import Messages
 from app.initializer import app, mongo
 
@@ -114,3 +115,70 @@ def save_area_information():
 
     mongo.db.api.insert_one(data)
     return jsonify({"msg": Messages.SUCCESS_SAVE_SUCCESSFULLY('Area Information')})
+
+
+@area_information.route("/tree", methods=["GET"])
+# @jwt_required()
+@swag_from({
+    'tags': ['Area Information'],
+    'summary': 'Create a new area information entry',
+    'description': 'Add a new area information record to the database.',
+    'responses': {
+        201: {'description': 'Area information created successfully'},
+        400: {'description': Messages.ERROR_INVALID_DATA('Area Information')}
+    }
+})
+def get_tree_information():
+    try:
+        filters = {}
+        params = request.args
+
+        area_id = params.get('area_id', False)
+        start_date = params.get('start_date', '2000-01-01')
+        end_date = params.get('end_date', str(datetime.now().strftime('%Y-%m-%d')))
+
+        msg = {
+            'area id': area_id,
+            'start date': start_date,
+            'end date': end_date
+        }
+
+        if params.get('area_id', False):
+            filters['area_id'] = int(params['area_id'])
+
+        if start_date or end_date:
+            filters['measurement_date'] = {
+                "$gte": start_date,
+                "$lt": end_date
+            }
+
+        pipeline = [
+            {"$match": filters},
+            {
+                "$group": {
+                    "_id": {"$substr": ["$measurement_date", 0, 7]},
+                    "total_number_of_trees_lost": {"$sum": "$number_of_trees_lost"},
+                    "total_average_tree_growth_cm": {"$sum": "$average_tree_growth_cm"},
+                    "total_trees_alive_so_far": {"$sum": "$trees_alive_so_far"},
+                    "total_tree_survival_rate": {"$sum": "$tree_survival_rate"}
+                }
+            },
+            {"$sort": {"_id": 1}},
+            {"$project": {
+                "_id": 0, 
+                "measurement_date": "$_id", 
+                "total_number_of_trees_lost": 1,
+                "total_average_tree_growth_cm": 1,
+                "total_trees_alive_so_far": 1,
+                "total_tree_survival_rate": 1
+                }}
+        ]
+
+        tree_info = list(mongo.db.api.aggregate(pipeline))
+        return jsonify(tree_info)
+
+    except KeyError as error:
+        abort(400, description=Messages.ERROR_INVALID_DATA('Area Information'))
+    except Exception as error:
+        abort(500, description=Messages.UNKNOWN_ERROR('Area Information'))
+
