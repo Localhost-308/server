faac787bc81a807e506258625cc42616ce0cc795
Lukas <lkfernando12345@gmail.com>
2025-03-21 10:48:00 -0300

    feat Task 50: ajustando no swagger para mostrar 0.1002 e não 10.02

--- app/controllers/area_information.py
+++ app/controllers/area_information.py
@@ -307,7 +307,7 @@ def get_tree_information():
                     'type': 'object',
                     'properties': {
                         'measurement_date': {'type': 'string', 'example': '2025-03'},
-                        'avg_soil_fertility_index_percent': {'type': 'number', 'example': 75.2}
+                        'avg_soil_fertility_index_percent': {'type': 'number', 'example': 0.7502}
                     }
                 }
             }
