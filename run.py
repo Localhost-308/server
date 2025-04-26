4322b91ca3308f951c0e19e9f48604373b192ee2
Lukas <lkfernando12345@gmail.com>
2025-04-26 18:11:19 -0300

    feat Task 79: finalizando a portabilidade

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app.controllers import area, company, localization, user, area_information, import_file, reforestation_stage, environment_threats
+from app.controllers import area, company, localization, user, area_information, import_file, reforestation_stage, environment_threats, portability
 from app.initializer import app
 from app.database.sqlite import init_sqlite_db
 
@@ -11,3 +11,4 @@ app.register_blueprint(area_information.area_information)
 app.register_blueprint(import_file.files)
 app.register_blueprint(reforestation_stage.reforestation)
 app.register_blueprint(environment_threats.environment_threats)
+app.register_blueprint(portability.portability)
